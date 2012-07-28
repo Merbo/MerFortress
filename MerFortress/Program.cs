5bedc1dd046eb3feab5fdfcf5577478df42b84a1
Merbo <carrethan02@gmail.com>
2012-07-28 17:16:38 -0400

    Initial Commit

---
+++ MerFortress/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MerFortress
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
